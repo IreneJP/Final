a5156f3959db76c87f817c16299a578f1be3784c
Irene J.P <airinsos@gmail.com>
2020-07-07 18:44:48 +0200

    Elimino comentarios y dejo solo la funcion calculadora

--- R1.js
+++ R1.js
@@ -1,7 +1,3 @@
-/*Construye una función “calculadora()” que reciba como parámetros de entrada: • Tipo de operación • Operadores (siempre 2 operadores para hacerlo más sencillo).
-Las operaciones permitidas son: suma (“sum”), resta (“subs”), multiplicación (“mult”) y división (“div”). 
-Dependiendo del identificador de la operación, y los parámetros de entrada, la función debe decidir qué operación realizar y devolvernos un resultado válido.
-*/
 
 
 function calculadora (tipoOperacion, operador1, operador2 ){
@@ -22,4 +18,3 @@ if (tipoOperacion == "suma"){
 
 calculadora("multiplicacion", 5, 4);
 console.log(calculadora("multiplicacion", 5, 4))
-// esto es un comentario para el reto2 de GIT
